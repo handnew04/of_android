3d43b6814ee604fd939bf1eacc372350d4389d70
handnew <handnew04@gmail.com>
2022-05-02 09:21:36 +0900

    feat: custom ui 작업중

--- app/src/main/java/kr/onekey/of/ui/custom/GreenLineEditText.kt
+++ app/src/main/java/kr/onekey/of/ui/custom/GreenLineEditText.kt
@@ -1,4 +1,17 @@
 package kr.onekey.of.ui.custom
 
-class GreenLineEditText {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class GreenLineEditText : AppCompatEditText {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+
 }
