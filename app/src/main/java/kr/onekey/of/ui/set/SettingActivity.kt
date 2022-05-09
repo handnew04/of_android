da66d9bfd0c02026e72893d7c318e9fe7aa25c5b
yujung <yujung.son@onekey.kr>
2022-05-09 19:14:31 +0900

    feat: SettingActivity setListener 추가 #5

--- app/src/main/java/kr/onekey/of/ui/set/SettingActivity.kt
+++ app/src/main/java/kr/onekey/of/ui/set/SettingActivity.kt
@@ -1,4 +1,63 @@
 package kr.onekey.of.ui.set
 
-class SettingActivity {
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
+import kr.onekey.of.R
+import kr.onekey.of.base.BaseActivity
+import kr.onekey.of.databinding.ActivitySettingBinding
+import kr.onekey.of.databinding.DialogChoiceBinding
+import kr.onekey.of.ui.PictureDialog
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class SettingActivity :
+   BaseActivity<ActivitySettingBinding, SettingViewModel>(R.layout.activity_setting) {
+   override val viewModel: SettingViewModel by viewModel()
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+
+      setListeners()
+   }
+
+   private fun setListeners() {
+      binding.apply {
+         ivProfile.setOnClickListener {
+            showDialog()
+         }
+         tvSave.setOnClickListener {
+
+         }
+         tvCancel.setOnClickListener {
+
+         }
+      }
+   }
+
+   private fun showDialog() {
+      val dialogBinding: DialogChoiceBinding =
+         DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.dialog_choice, null, false)
+
+      PictureDialog(
+         dialogBinding,
+         cameraButtonClickListener = object : PictureDialog.DialogOnClickListener {
+            override fun onClick() {
+               takePicture()
+            }
+
+         },
+         galleryButtonClickListener = object : PictureDialog.DialogOnClickListener {
+            override fun onClick() {
+               getPicture()
+            }
+         }).show()
+   }
+
+   private fun takePicture() {
+
+   }
+
+   private fun getPicture() {
+
+   }
 }
