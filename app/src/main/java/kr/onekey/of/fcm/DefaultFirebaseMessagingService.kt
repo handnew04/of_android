d269c078f9d4711f42f7c7e76b2ec9a73ea5c547
handnew <handnew04@gmail.com>
2022-05-12 15:24:26 +0900

    feat: fcm 준비중 #4

--- app/src/main/java/kr/onekey/of/fcm/DefaultFirebaseMessagingService.kt
+++ app/src/main/java/kr/onekey/of/fcm/DefaultFirebaseMessagingService.kt
@@ -1,12 +1,26 @@
 package kr.onekey.of.fcm
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kr.onekey.of.R
+import kr.onekey.of.ui.main.MainActivity
 
 class DefaultFirebaseMessagingService : FirebaseMessagingService(){
    override fun onMessageReceived(message: RemoteMessage) {
       super.onMessageReceived(message)
 
+      val messageBody = message.data["body"]
+      if (!messageBody.isNullOrBlank()) {
+         sendNotification(messageBody)
+      }
    }
 
    override fun onNewToken(token: String) {
@@ -15,6 +29,31 @@ class DefaultFirebaseMessagingService : FirebaseMessagingService(){
    }
 
    private fun sendNotification(messageBody: String) {
-      
+      val intent = Intent(this, MainActivity::class.java)
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+      val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+         PendingIntent.FLAG_ONE_SHOT)
+
+      val channelId = getString(R.string.default_notification_channel_id)
+      val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+      val notificationBuilder = NotificationCompat.Builder(this, channelId)
+         .setSmallIcon(R.drawable.ic_mail)
+         .setContentTitle("알림이 도착했습니다룽")
+         .setContentText(messageBody)
+         .setAutoCancel(true)
+         .setSound(defaultSoundUri)
+         .setContentIntent(pendingIntent)
+
+      val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+      // Since android Oreo notification channel is needed.
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+         val channel = NotificationChannel(channelId,
+            "Channel human readable title",
+            NotificationManager.IMPORTANCE_DEFAULT)
+         notificationManager.createNotificationChannel(channel)
+      }
+
+      notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
    }
 }
